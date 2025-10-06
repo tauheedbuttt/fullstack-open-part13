f9ebab2a34f1e1bc3264d3b2ea6f0bb0fb6a2d97
tauheedbuttt <tauheedbutt@gmail.com>
2025-10-06 17:48:56 +0300

    Exercise 13.4.

--- blog-backend/index.js
+++ blog-backend/index.js
@@ -1,25 +1,51 @@
 require("dotenv").config();
-const { Sequelize } = require("sequelize");
+
+const { Sequelize, Model, DataTypes } = require("sequelize");
+const express = require("express");
+const app = express();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL);
 
-const main = async () => {
+class Blog extends Model {}
+Blog.init(
+  {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    author: { type: DataTypes.TEXT },
+    url: { type: DataTypes.TEXT, allowNull: false },
+    title: { type: DataTypes.TEXT, allowNull: false },
+    likes: { type: DataTypes.INTEGER, defaultValue: 0 },
+  },
+  { sequelize, underscored: true, timestamps: false, modelName: "blog" }
+);
+
+Blog.sync();
+
+app.get("/api/blogs", async (req, res) => {
+  const blogs = await Blog.findAll();
+  res.json(blogs);
+});
+
+app.post("/api/blogs", async (req, res) => {
   try {
-    await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-
-    const blogs = await sequelize.query("SELECT * FROM blogs", {
-      type: Sequelize.QueryTypes.SELECT,
-    });
-
-    blogs.forEach((blog) => {
-      console.log(`${blog.author}: '${blog.title}', ${blog.likes} likes`);
-    });
-
-    sequelize.close();
+    const blog = await Blog.create(req.body);
+    return res.json(blog);
   } catch (error) {
-    console.error("Unable to connect to the database:", error);
+    return res.status(400).json({ error });
   }
-};
+});
 
-main();
+app.delete("/api/blogs/:id", async (req, res) => {
+  const { id } = req.params;
+  const blog = await Blog.findByPk(id);
+  if (blog) {
+    await blog.destroy();
+    res.status(204).end();
+  } else {
+    res.status(404).json({ error: "Blog not found" });
+  }
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
